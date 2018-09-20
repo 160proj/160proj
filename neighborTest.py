6ba448844748898f2f04eaad0e7a64bfd4c68faa
Jadon Hansell <alcamore@gmail.com>
2018-09-19 17:52:51 -0700

    Separate neighbor and flooding tests

--- neighborTest.py
+++ neighborTest.py
@@ -8,7 +8,7 @@ def main():
     s.runTime(1);
 
     # Load the the layout of the network.
-    s.loadTopo("long_line.topo");
+    s.loadTopo("example.topo");
 
     # Add a noise model to all of the motes.
     s.loadNoise("no_noise.txt");
@@ -19,19 +19,19 @@ def main():
     # Add the main channels. These channels are declared in includes/channels.h
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
-    # s.addChannel(s.FLOODING_CHANNEL);
+    # s.addChannel(s.NEIGHBOR_CHANNEL);
 
     # After sending a ping, simulate a little to prevent collision.
     s.runTime(1);
 
-    s.ping(2, 3, "Hello, World");
+    s.neighborDMP(2);
+    s.runTime(1);
+
+    s.neighborDMP(1);
     s.runTime(5);
 
-    s.ping(1, 10, "Hi!");
-    s.runTime(5);
-
-    # s.neighborDMP(5);
-    # s.runTime(1);
+    s.neighborDMP(5);
+    s.runTime(1);
 
 if __name__ == '__main__':
     main()
