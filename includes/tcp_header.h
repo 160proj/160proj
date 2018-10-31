3a2979324f723ab857e54ad998540c7976fa3e0d
Jadon Hansell <alcamore@gmail.com>
2018-10-31 16:37:59 -0700

    Add TCP header struct

---
+++ includes/tcp_header.h
@@ -0,0 +1,19 @@
+#ifndef TCP_HEADER_H
+#define TCP_HEADER_H
+
+enum {
+    TCP_HEADER_SIZE = 9
+    TCP_PAYLOAD_SIZE = 20 - TCP_HEADER_SIZE
+}
+
+typedef nx_struct tcp_header {
+    nx_uint8_t src_port;
+    nx_uint8_t dest_port;
+    nx_uint16_t seq;
+    nx_uint16_t ack;
+    nx_uint16_t advert_window;
+    nx_uint8_t flags;
+    nx_uint8_t payload[TCP_PAYLOAD_SIZE]
+} tcp_header;
+
+#endif // TCP_HEADER_H
